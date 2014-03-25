3f37579defebdd85c6518191b41cbd9c238e2e95
CharlesGarrocho <charles.garrocho@gmail.com>
2014-03-24 21:00:40 -0300

    adicionado; estrutura evaluations. #1

--- PAA/TP1_MERGESORT/rank.c
+++ PAA/TP1_MERGESORT/rank.c
@@ -2,3 +2,11 @@
 #include <stdlib.h>
 #include <time.h>
 #include <assert.h>
+
+typedef struct
+{
+    int id; /* unique identifier of the candidate */
+    /* there are two different scores,  sorting should be done in lexicographic order (major, minor) */
+    int major; /* score in major subjects */
+    int misc; /* score in miscellaneous subjects, used only to break ties */
+} Evaluation;
