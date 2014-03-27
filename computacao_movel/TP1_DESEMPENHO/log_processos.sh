ff6112c81cd78374bc7c2c30f52baa0d071573e5
CharlesGarrocho <charles.garrocho@gmail.com>
2014-03-27 14:24:02 -0300

    adicionado: execução do script com argumento. #2

--- computacao_movel/TP1_DESEMPENHO/log_processos.sh
+++ computacao_movel/TP1_DESEMPENHO/log_processos.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
-ps aux | grep chrome | sed '2,$ d' | awk '{print $3,";",$4}'
+if [ "$1" = "" ]; then
+    echo "./log_processos.sh <nome_processo>"
+fi
+
+if [ "$1" != "" ]; then
+   ps aux | grep chrome | sed '2,$ d' | awk '{print $3,";",$4}'
+fi
+#ps aux | grep chrome | sed '2,$ d' | awk '{print $3,";",$4}'
