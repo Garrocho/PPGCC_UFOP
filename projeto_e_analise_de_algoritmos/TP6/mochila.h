00d08351564ae131d23e372c131b83febfd62d2a
CharlesGarrocho <charles.garrocho@gmail.com>
2014-05-28 22:02:10 -0300

    adicionado: função para contar itens selecionados.

--- projeto_e_analise_de_algoritmos/TP6/mochila.h
+++ projeto_e_analise_de_algoritmos/TP6/mochila.h
@@ -10,6 +10,8 @@ int top_down(int, int, int*, int*, int**, int**);
 
 int bottom_up(int, int, int*, int*, int**, int**);
 
+int count_picks(int, int, int *, int **);
+
 void print_picks(int, int, int*, int**, FILE*);
 
 #endif /* MOCHILA_H */
