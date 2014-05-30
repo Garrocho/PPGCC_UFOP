ecac87ce51ef68740e1a40b31df3c45411c0a409
CharlesGarrocho <charles.garrocho@gmail.com>
2014-05-29 23:08:55 -0300

    adicionado: problema do bobos lanches.

--- projeto_e_analise_de_algoritmos/TP6/bobos.c
+++ projeto_e_analise_de_algoritmos/TP6/bobos.c
@@ -1,73 +1,145 @@
 #include <time.h>
 #include <stdio.h>
 #include <stdlib.h>
+#define max(a, b) (a > b ? a : b)
 
-int* gera_vetor(int, int);
+int top_down(int index, int size, int *weights, int *values, int **matrix, int **picks);
 
-int solucao(int n, int k, int *distancias, int *lucros);
+int count_picks(int item, int size, int *weights, int **picks);
 
-int main(int argc, char **argv) {
+void print_picks(int item, int size, int *weights, int **picks, FILE *arquivo);
 
-	if (argc != 3) {
-		printf("Argumentos Inválidos...\nUtilize: %s <tamanho_vetor> <distancia>\n", argv[0]);
+int main(int argc, const char * argv[])
+{
+	if (argc != 2) {
+		printf("Uso: ./a.out <arquivos>\n");
 		exit(EXIT_FAILURE);
 	}
-	int tamanho = atoi(argv[1]);
-	int distancia = atoi(argv[2]);
-	
-	int* distancias = gera_vetor(tamanho, distancia);
-	int* lucros = gera_vetor(tamanho, distancia);
-	int lucro = solucao(tamanho, distancia/2, distancias, lucros);
-	printf("%d\n", lucro);
 
+	else {
+		const char* infile = argv[1];
+		FILE *arquivo;
+		FILE *arquivo2;
+		FILE* fp = fopen(infile, "r");
+		if (fp == NULL)
+		{
+			printf("Nao pode abrir %s.\n", infile);
+			exit(EXIT_FAILURE);
+		}
+
+		int n, W;
+		fscanf(fp, "%d", &n);
+		fscanf(fp, "%d", &W);
+
+		int vals[n], wts[n], **matrix, **picks;
+		int i = 0, j = 0, index = 0;
+
+		matrix = (int**)calloc(n, sizeof(int*));
+		picks = (int**)calloc(n, sizeof(int*));
+
+		for (i=0; i < n; i++) {
+			matrix[i] = (int*)calloc(W+1, sizeof(int));
+			picks[i] = (int*)calloc(W+1, sizeof(int));
+		}
+
+		while (!feof (fp) && index < n) {
+			fscanf(fp, "%d", &j);
+			wts[index++] = j;
+		}
+
+		index = 0;
+		while (!feof (fp) && index < n) {
+			fscanf(fp, "%d", &i);
+			vals[index++] = i;
+		}
+
+		char endereco[100];
+		sprintf(endereco, "%s.sol", infile);
+		arquivo = fopen(endereco, "w");
+
+		clock_t start = clock();
+		fprintf(arquivo, "Top Down\nLucro Otimo = %d\n", top_down(n-1, W, wts, vals, matrix, picks));
+		clock_t end = clock();
+		double cpuTime = ((double) (end - start)) / CLOCKS_PER_SEC;
+		fprintf(arquivo, "Tempo Total da execucao: %.2f\n", cpuTime);
+		fprintf(arquivo, "Quantidade de Selecionados: %d\n", count_picks(n-1, W, wts, picks));
+		fprintf(arquivo, "Selecionados:\n");
+		print_picks(n-1, W, wts, picks, arquivo);
+
+		fclose(fp);
+		fclose(arquivo);
+
+		for (i=0; i < n; i++) {
+			free(matrix[i]);
+			free(picks[i]);
+		}
+		free(matrix);
+		free(picks);
+	}
 	return EXIT_SUCCESS;
 }
 
-int solucao(int n, int k, int* distancias, int* lucros) {
-	int i;
-	int* solucao = (int*)malloc(sizeof(int)*n);
+int top_down(int index, int size, int *weights, int *values, int **matrix, int **picks) {
+	int take, dontTake;
 
-	for (i=0; i<n; i++) {
-		solucao[i] = 0;
-	}
-	
-	for (i=1; i<n-1; i++) {
-		if ((distancias[i]-distancias[i-1]) >= k)
-			solucao[i] = 1;
-	}
-}
+	take = dontTake = 0;
 
-int bottom_up(int n, int k, int size, int *distancias, int *lucros, int **matrix, int **picks) {
-	int i, j;
+	if (matrix[index][size] != 0)
+		return matrix[index][size];
 
-	for (i=1; i <= n; i++) {
-		for (j=0; j <= size; j++) {
-			if (distancias[i-1] <= j) {
-				matrix[i][j] = max(matrix[i-1][j], lucros[i-1] + matrix[i-1][j-distancias[i-1]]);
-
-				if ((lucros[i-1] + matrix[i-1][j-distancias[i-1]] > matrix[i-1][j]) && 
-					picks[i][j] = 1;
-				else
-					picks[i][j] = -1;
-			}
-			else {
-				picks[i][j] = -1;
-				matrix[i][j] = matrix[i-1][j];
-			}
+	if (index == 0) {
+		if (weights[0] <= size ) {
+			picks[index][size] = 1;
+			matrix[index][size] = values[0];
+			return values[0];
+		}
+		else {
+			picks[index][size] = -1;
+			matrix[index][size] = 0;
+			return 0;
 		}
 	}
-	return matrix[n][size];
+
+	if (weights[index] <= size)
+		take = values[index] + top_down(index-1, size-weights[index], weights, values, matrix, picks);
+
+	dontTake = top_down(index-1, size, weights, values, matrix, picks);
+	matrix[index][size] = max(take, dontTake);
+
+	if (take > dontTake)
+		picks[index][size]=1;
+	else
+		picks[index][size]=-1;
+
+	return matrix[index][size];
 }
 
-int* gera_vetor(int tamanho, int distancia) {
-	int i;
-	int* vetor = (int*)malloc(sizeof(int)*tamanho);
 
-	srand(time(NULL));
-
-	vetor[0] = 1+(rand()%distancia);
-	for (i=1; i<tamanho; i++) {
-		vetor[i] = 1 + (rand()%distancia) + vetor[i-1];
+int count_picks(int item, int size, int *weights, int **picks) {
+	int count = 0;
+	while (item >= 0 && size >= 0) {
+		if (picks[item][size] == 1) {
+			count++;
+			item--;
+			size -= weights[item];
+		}
+		else {
+			item--;
+		}
 	}
-	return vetor;
+	return count;
+}
+
+void print_picks(int item, int size, int *weights, int **picks, FILE *arquivo) {
+
+	while (item >= 0 && size >= 0) {
+		if (picks[item][size] == 1) {
+			fprintf(arquivo, "%d\n", item);
+			item--;
+			size -= weights[item];
+		}
+		else {
+			item--;
+		}
+	}
 }
