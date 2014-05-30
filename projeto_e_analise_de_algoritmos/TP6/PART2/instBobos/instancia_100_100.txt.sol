462aa061d74d66e62fc142b127c1ae3fbe644e5a
CharlesGarrocho <charles.garrocho@gmail.com>
2014-05-30 02:05:34 -0300

    adicionado: parte 2 do projeto.

---
+++ projeto_e_analise_de_algoritmos/TP6/PART2/instBobos/instancia_100_100.txt.sol
@@ -0,0 +1,29 @@
+Top Down
+Lucro Otimo = 3183
+Tempo Total da execucao: 0.00
+Quantidade de Selecionados: 24
+Selecionados:
+99
+94
+90
+85
+80
+75
+71
+66
+62
+57
+50
+46
+43
+38
+34
+30
+24
+20
+15
+10
+6
+2
+1
+0
