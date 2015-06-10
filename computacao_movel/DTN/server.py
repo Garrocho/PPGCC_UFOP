ea2a58ee29833fb9f077f3e66059723ec88fdb70
CharlesGarrocho <charles.garrocho@gmail.com>
2015-06-09 23:08:06 -0300

    adicionado: put and get

--- computacao_movel/DTN/server.py
+++ computacao_movel/DTN/server.py
@@ -1,24 +1,43 @@
 import os
+import json
 import time
 import socket
-from json import loads
 from threading import Thread
+
+IPS = []
 
 def trata_cliente(conexao, endereco):
     requisicao = conexao.recv(1024)
+    print requisicao
     if requisicao == 'LIST':
-        arqs = of.listdir('/etc/black/carro/arquivos/')
-        conexao.send(arqs)
+        arqs = os.listdir('/etc/black/garagem/arquivos/')
+        conexao.send(json.dumps(arqs))
     elif requisicao == 'GET':
+        arqs = os.listdir('/etc/black/garagem/arquivos/')
         arquivo = conexao.recv(1024)
-        arq = open(arquivo, 'w')
-        tempos = 0
-        while 1:
-            dados = conexao.recv(1024)
-            if not dados:
-                break
-            arq.write(dados)
-        arq.close()
+        if arquivo in arqs:
+            fp = open('/etc/black/garagem/arquivos/{0}'.format(arquivo), 'r')
+            strng = fp.read(1024)
+            while strng:
+                conexao.send(strng)
+                strng = fp.read(1024)
+    elif requisicao == 'PUT':
+        conexao.send('OK')
+        arqs = os.listdir('/etc/black/garagem/arquivos/')
+        arquivo = conexao.recv(1024)
+        print arquivo
+        print arqs
+        if arquivo not in arqs:
+            conexao.send('TRUE')
+            arq = open('/etc/black/garagem/arquivos/{0}'.format(arquivo), 'w')
+            while 1:
+                dados = conexao.recv(1024)
+                if not dados:
+                    break
+                arq.write(dados)
+            arq.close()
+        else:
+            conexao.send('FALSE')
     conexao.close()
 
 
@@ -26,9 +45,9 @@ def loop_servidor():
     
     soquete = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     soquete.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    soquete.bind(('10.10.0.1', 5555))
+    soquete.bind(('127.0.0.1', 5555))
     soquete.listen(10)
-
+    global IPS
     # Fica aqui aguardando novas conexoes.
     while True:
 
@@ -37,6 +56,8 @@ def loop_servidor():
         novaConexao = []
         novaConexao.append(conexao[0])
         novaConexao.append(conexao[1])
+        if conexao[1] not in IPS:
+            IPS.append(conexao[1])
         Thread(target=trata_cliente, args=(novaConexao)).start()
 
 
